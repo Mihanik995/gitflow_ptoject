128b17a1b8fa503309b052cc1a874f081aeb3869
Mikhail Mironov <mihanik995@gmail.com>
2023-11-21 13:32:53 +0200

    add str_upper func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
+def str_upper(string):
+    """makes each letter big"""
+    return string.upper()
+
 def str_title(string):
+    """makes first letter of each word big"""
     return string.title()
